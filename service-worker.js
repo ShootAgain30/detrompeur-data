fc620c1398c91532ffa9cbac01189b73596e1cb1
ShootAgain30 <51975378+ShootAgain30@users.noreply.github.com>
2026-03-05 23:42:22 +0100

    Update service worker cache name and assets

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,23 @@
-const CACHE_NAME = 'shootagain-v1';
+const CACHE_NAME = 'detrompeur-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/DS_Icone4_copie.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './Icon.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
